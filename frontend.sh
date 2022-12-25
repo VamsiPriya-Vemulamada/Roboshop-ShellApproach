790a619f43d51c102dadb0f7c5eb6354e1650591
VamsiPriya-Vemulamada <vamsipiryavemulamada@outlook.com>
2022-12-25 14:59:48 -0500

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 yum install ngnix -y
 
 #downloading the code
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#downloading the content
+#download the content
 cd /usr/share/ngnix/html
 rm -rf *
 unzip /tmp/fronntend.zip
