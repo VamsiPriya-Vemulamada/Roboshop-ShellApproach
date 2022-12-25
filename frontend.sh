0c6cfad9ac3fbd4edf1192a1b28466cc14d1afca
VamsiPriya-Vemulamada <vamsipiryavemulamada@outlook.com>
2022-12-25 14:38:05 -0500

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,5 @@
 yum install ngnix -y
-systemctl enable nginx
-systemctl start ngnix
+
 #downloading the code
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 #downloading the content
@@ -10,4 +9,6 @@ unzip /tmp/fronntend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
+systemctl enable nginx
+systemctl start ngnix
 systemctl restart nginx
