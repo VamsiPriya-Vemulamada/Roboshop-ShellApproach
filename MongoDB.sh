834c413929ad8dace9fb9d40c300ab112f4ccd1d
VamsiPriya-Vemulamada <vamsipiryavemulamada@outlook.com>
2022-12-25 16:58:46 -0500

    mongo script

---
+++ MongoDB.sh
@@ -0,0 +1,11 @@
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+
+#Installing&Start
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+#Updating the IP address by using SED editor, this is used for automation rather than Vi editor
+sed -i -e 's/127.0.0.1 0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
